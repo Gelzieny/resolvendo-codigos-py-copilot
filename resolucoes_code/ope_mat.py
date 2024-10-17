c4fc2deda37636c6c7af5b23ece718c11290b674
Gelzieny R. Martins <gelzieny@gmail.com>
2024-10-17 16:56:17 -0300

    finalizando o desafio

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+dado1 = int(input("Digite o primeiro dado: "))
+dado2 = int(input("Digite o segundo dado: "))
+
+resultado = dado1 + dado2 
+
+print("Resultado da soma", resultado)
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,23 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+operacoes = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+}
+
+
+dado1 = int(input("Digite o primeiro dado: "))
+dado2 = int(input("Digite o segundo dado: "))
+
+operacao = input("Digite a operação que deseja realizar (+, -, *, /): ")
+
+
+if operacao in operacoes:
+    resultado = operacoes[operacao](dado1, dado2)
+else:
+    print("Operação inválida")
+
+# Exibe o resultado
+print("Resultado da soma:", resultado)
